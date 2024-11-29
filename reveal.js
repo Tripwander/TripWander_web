5dee01e8467eebebbf79e3c4966fc54632a17948
Tripwander <vinicioszubreski7@gmail.com>
2024-11-29 10:07:00 -0300

    TripWander_web

---
+++ reveal.js
@@ -0,0 +1,67 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+// topo do site
+
+revelar.reveal('.efeito-h1-sub',
+{
+    duration: 1500,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-p1-sub',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay: 200
+    })
+
+
+// titulos 
+
+revelar.reveal('.h2-2',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 200
+    })
+
+
+revelar.reveal('.h2-1',
+    {
+        duration: 1500,
+        distance: '90px'
+    })
+
+revelar.reveal('.imagem1',
+    {
+        duration: 1500,
+        distance: '90px'
+    })
+
+revelar.reveal('.p3',
+        {
+            duration: 1500,
+            distance: '90px',
+            delay: 100
+        })
+
+revelar.reveal('.p2',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay: 100
+    })
+
+revelar.reveal('.h2-3',
+    {
+        duration: 1500,
+        distance: '100px'
+    })
+
+revelar.reveal('.p4',
+    {
+        duration: 1500,
+        distance: '100px',
+        delay: 500
+    })   
